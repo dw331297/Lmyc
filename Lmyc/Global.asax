9c2af4a1aa6e29a4a094eaeadecb5c44de957981
TheBoognish <dwatson76@my.bcit.ca>
2018-02-09 16:54:10 -0800

    Add project files.

---
+++ Lmyc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lmyc.MvcApplication" Language="C#" %>
---
+++ Lmyc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lmyc.Startup))]
+namespace Lmyc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
