8ed64811ff4033e8f96677b6380811d3da8c675f
TheBoognish <dwatson76@my.bcit.ca>
2018-02-18 15:17:36 -0800

    changes

--- Lmyc/Models/ManageViewModels.cs
+++ Lmyc/Models/ManageViewModels.cs
@@ -27,9 +27,7 @@ namespace Lmyc.Models
 
     public class SetPasswordViewModel
     {
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
+        [Required]                       
         [Display(Name = "New password")]
         public string NewPassword { get; set; }
 
