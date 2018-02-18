dc8d279707a26b809e28044ec8863cd2880dd86b
llow <llowenstein@my.bcit.ca>
2018-02-18 10:05:09 -0800

    last migration

---
+++ Lmyc/Migrations/BoatMigrations/201802181804208_seventh.cs
@@ -0,0 +1,18 @@
+namespace Lmyc.Migrations.BoatMigrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class seventh : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Boats", "RecordCreationDate", c => c.DateTime(nullable: false, precision: 7, storeType: "datetime2"));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Boats", "RecordCreationDate", c => c.DateTime(nullable: false));
+        }
+    }
+}
