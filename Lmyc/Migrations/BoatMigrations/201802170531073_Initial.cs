e1d020381ce4225570b66020eeafd6d4efd19fde
llow <llowenstein@my.bcit.ca>
2018-02-18 10:03:36 -0800

    migration changes

--- Lmyc/Migrations/BoatMigrations/201802170531073_Initial.cs
+++ Lmyc/Migrations/BoatMigrations/201802170531073_Initial.cs
@@ -7,6 +7,24 @@ namespace Lmyc.Migrations.BoatMigrations
     {
         public override void Up()
         {
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.Boats", "CreatedBy", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.Boats", new[] { "CreatedBy" });
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
+
             CreateTable(
                 "dbo.Boats",
                 c => new
